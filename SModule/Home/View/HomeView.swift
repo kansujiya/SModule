ec8747b777d2e880bd8fb63f0cd67b91dd947865
Kansujiya <sureshkansujiya@gmail.com>
2017-04-30 01:24:28 +0530

    Home Module Added

---
+++ SModule/Home/View/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  SModule
+//
+//  Created by Suresh Kansujiya on 30/04/17.
+//  Copyright © 2017 Suresh Kansujiya. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
